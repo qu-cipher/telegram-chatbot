428ddfc85913911152feb20da80a81dbd582de63
qu-cipher <ali.code88s@gmail.com>
2025-02-16 22:16:04 +0330

    update: index.ts: test hello

--- supabase/functions/bot/index.ts
+++ supabase/functions/bot/index.ts
@@ -1,7 +1,8 @@
 // deno-lint-ignore-file
 import { Bot, webhookCallback } from "https://deno.land/x/grammy@v1.33.0/mod.ts";
+import { BOT_SECRET } from "../_shared/vars.ts";
 
-const bot = new Bot("BOT_SECRET");
+const bot = new Bot(BOT_SECRET);
 
 bot.command("start", (ctx) => ctx.reply("Hello!"));
 /**
